44714f49f214c88ac9c869af474e40b16462fdd9
navydragon <navydragon@inbox.ru>
2018-01-24 00:51:02 +0300

    second

---
+++ resources/views/admin/welcome.blade.php
@@ -0,0 +1,1 @@
+@extends ('parts/admin/main')
---
+++ resources/views/parts/admin/main_menu.blade.php
@@ -0,0 +1,16 @@
+					<!-- SECOND MAIN LIST -->
+					<h3>MORE</h3>
+					<ul class="nav nav-list">
+						<li>
+							<a href="admin1/kst">
+								<i class="main-icon fa fa-list"></i>
+								<span>КСТ</span>
+							</a>
+						</li>
+						<li>
+							<a href="../../HTML_BS4/start.html">
+								<i class="main-icon fa fa-link"></i>
+								<span class="label label-danger pull-right">PRO</span> <span>Frontend</span>
+							</a>
+						</li>
+					</ul>
